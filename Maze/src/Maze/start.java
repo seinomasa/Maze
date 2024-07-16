e72a591841f7970339f5f49061834ce448457050
osamu <kopoandms@gmail.com>
2024-07-16 11:03:51 +0900

    訂正版

---
+++ Maze/src/Maze/start.java
@@ -0,0 +1,14 @@
+package Maze;
+public class start extends MyFrame {
+	
+	static boolean spacePressed;
+ 
+	public static void main(String[] args) {
+		MyFrame maze =new Maze2(15,15);
+		//maze.show();
+	}
+	
+ 
+ 
+}
+
